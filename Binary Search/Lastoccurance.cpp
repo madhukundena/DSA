ff7659cf1c986a796ffd00118d83cadbf8be847c
Madhu Kundena <55804416+madhukundena@users.noreply.github.com>
2022-03-19 19:12:40 +0530

    added missing

---
+++ Binary Search/Lastoccurance.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lastbs(int A[],int n, int key){
+    int low=0,high=n-1,mid=0,posans=-1;    
+    while(low<=high){
+        mid=(low+high)/2;
+        if(A[mid]==key){
+            posans=mid;
+            low=mid+1;
+        }
+        else if(A[mid]<key){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+
+    return posans;
+}
+   
+int main(){ 
+    int n=10;
+    int arr[]={1,2,3,4,5,10,10,10,10,10};
+    int key=10;
+    cout<<"The answer is"<<lastbs(arr,n,key);   
+    return 0;
+}
+
---
+++ Binary Search/notimerotbs.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void nbs(int arr[],int n){
+    int mid,prev,next,low=0,high=n-1;
+    while(low<=high)
+    {   
+        mid=(low+high)/2;
+        prev=(mid-1+n)%n;
+        next=(mid+1)%n;
+        if(arr[mid]<arr[prev]&&arr[mid]<arr[next]){ 
+            break;
+        }
+        else if(arr[high]>arr[mid]){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    cout<<mid<<" times rotated";
+
+}
+    
+
+
+int main(){ 
+    int n=10;
+    int arr[]={3,4,5,6,7,8,9,10,1,2};
+    int key=10;
+    
+    nbs(arr,n);
+   
+    return 0;
+}
+
