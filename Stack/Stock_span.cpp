7875a07caaf97d540dc01cc7a04a2302af2b62be
Madhu Kundena <55804416+madhukundena@users.noreply.github.com>
2022-03-19 14:48:42 +0530

    19/03/2022

---
+++ Stack/Stock_span.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=10;
+    int arr[]={1,2,0,4,5,10,9,8,11,6};
+    cout<<"before hello"<<endl;
+    stack<int> s;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        if(s.size()==0){
+            v.push_back(-1);
+
+        }
+        else if(s.top()>arr[i]){
+            v.push_back(s.top());
+        }
+        else{
+            while(s.size()>0&&s.top()<arr[i]){
+                s.pop();
+                if(s.size()>0&&s.top()>arr[i]){
+                    v.push_back(s.top());
+                }
+            }
+            if(s.size()==0){
+                v.push_back(-1);
+            }
+        }
+        s.push(arr[i]);
+        //cout<<"stack top"<<s.top()<<endl;
+    }
+
+     //reverse(v.begin(), v.end());
+ 
+    
+    cout << "Reversed Vector: ";
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+   
+
+    return 0;
+}
